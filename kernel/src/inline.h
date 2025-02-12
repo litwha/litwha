6dc45f53c3e20dd8d05d4f61438a1060ef468546
Nasko-5 <sect0r.mainacc@gmail.com>
2025-02-12 22:53:43 +0200

    extract inline asm methods to serpate file

---
+++ kernel/src/inline.h
@@ -0,0 +1,3 @@
+#include <stdint.h>
+static inline void outb(uint16_t port, uint8_t val);
+static inline uint8_t inb(uint16_t port);
--- kernel/src/serial.c
+++ kernel/src/serial.c
@@ -1,42 +1,26 @@
 #include "string.h"
 #include <stdint.h>
 
+#include "inline.c"
 #include "serial.h"
-#define PORT 0x3f8          // COM1
+#define PORT 0x3f8 // COM1
 
-static inline uint8_t inb(uint16_t port)
+int init_serial()
 {
-    uint8_t ret;
-    __asm__ volatile ( "inb %w1, %b0"
-                   : "=a"(ret)
-                   : "Nd"(port)
-                   : "memory");
-    return ret;
-}
-
-static inline void outb(uint16_t port, uint8_t val)
-{
-    __asm__ volatile ( "outb %b0, %w1" : : "a"(val), "Nd"(port) : "memory");
-    /* There's an outb %al, $imm8 encoding, for compile-time constant port numbers that fit in 8b. (N constraint).
-     * Wider immediate constants would be truncated at assemble-time (e.g. "i" constraint).
-     * The  outb  %al, %dx  encoding is the only option for all other cases.
-     * %1 expands to %dx because  port  is a uint16_t.  %w1 could be used if we had the port number a wider C type */
-}
-
-int init_serial() {
-    outb(PORT + 1, 0x00);    // Disable all interrupts
-    outb(PORT + 3, 0x80);    // Enable DLAB (set baud rate divisor)
-    outb(PORT + 0, 0x03);    // Set divisor to 3 (lo byte) 38400 baud
-    outb(PORT + 1, 0x00);    //                  (hi byte)
-    outb(PORT + 3, 0x03);    // 8 bits, no parity, one stop bit
-    outb(PORT + 2, 0xC7);    // Enable FIFO, clear them, with 14-byte threshold
-    outb(PORT + 4, 0x0B);    // IRQs enabled, RTS/DSR set
-    outb(PORT + 4, 0x1E);    // Set in loopback mode, test the serial chip
-    outb(PORT + 0, 0xAE);    // Test serial chip (send byte 0xAE and check if serial returns same byte)
+    outb(PORT + 1, 0x00); // Disable all interrupts
+    outb(PORT + 3, 0x80); // Enable DLAB (set baud rate divisor)
+    outb(PORT + 0, 0x03); // Set divisor to 3 (lo byte) 38400 baud
+    outb(PORT + 1, 0x00); //                  (hi byte)
+    outb(PORT + 3, 0x03); // 8 bits, no parity, one stop bit
+    outb(PORT + 2, 0xC7); // Enable FIFO, clear them, with 14-byte threshold
+    outb(PORT + 4, 0x0B); // IRQs enabled, RTS/DSR set
+    outb(PORT + 4, 0x1E); // Set in loopback mode, test the serial chip
+    outb(PORT + 0, 0xAE); // Test serial chip (send byte 0xAE and check if serial returns same byte)
 
     // Check if serial is faulty (i.e: not same byte as sent)
-    if(inb(PORT + 0) != 0xAE) {
-          return 1;
+    if (inb(PORT + 0) != 0xAE)
+    {
+        return 1;
     }
 
     // If serial is not faulty set it in normal operation mode
@@ -45,29 +29,37 @@ int init_serial() {
     return 0;
 }
 
-int serial_received() {
+int serial_received()
+{
     return inb(PORT + 5) & 1;
 }
 
-char read_serial() {
-    while (serial_received() == 0);
+char read_serial()
+{
+    while (serial_received() == 0)
+        ;
 
     return inb(PORT);
 }
 
-int is_transmit_empty() {
+int is_transmit_empty()
+{
     return inb(PORT + 5) & 0x20;
 }
 
-void write_serial(char a) {
-    while (is_transmit_empty() == 0);
+void write_serial(char a)
+{
+    while (is_transmit_empty() == 0)
+        ;
 
-    outb(PORT,a);
+    outb(PORT, a);
 }
 
-void write_serial_str(char* str) {
+void write_serial_str(char *str)
+{
     int length = strlen(str);
-    for (int i = 0; i < length; i++) {
+    for (int i = 0; i < length; i++)
+    {
         write_serial(str[i]);
     }
 }
